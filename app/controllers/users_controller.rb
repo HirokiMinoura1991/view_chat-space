0a8a736bea662eafd3f19f3317c204f77fdd0eaa
HirokiMinoura1991 <hirokiminoura.tech@gmail.com>
2019-11-08 16:25:17 +0900

    initial comit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  def index
+    if params[:keyword] == ""
+      return nil 
+    else
+      @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+      respond_to do |format|
+        format.html
+        format.json
+        end
+    end
+  end
+  
   def edit
   end
 
