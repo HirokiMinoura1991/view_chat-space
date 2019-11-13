2a55b0932ce3aa03f4dca3692c8933269a868741
HirokiMinoura1991 <hirokiminoura.tech@gmail.com>
2019-11-13 20:28:23 +0900

    deoloytest

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :default_env,{
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
-set :linked_files, %w{ config/secrets.yml }
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
